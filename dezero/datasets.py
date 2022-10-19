ab5b248f3222ad7de511ca3a39e82b9393642eac
ne745 <kkpma0@gmail.com>
2022-10-19 22:00:55 +0900

    Step 49: データセットの前処理を実装

--- dezero/datasets.py
+++ dezero/datasets.py
@@ -4,8 +4,15 @@ import numpy as np
 
 
 class Dataset(ABC):
-    def __init__(self, train=True):
+    def __init__(self, train=True, transform=None, target_transform=None):
         self.train = train
+        self.transform = transform
+        self.target_transform = target_transform
+        if self.transform is None:
+            self.transform = lambda x: x
+        if self.target_transform is None:
+            self.target_transform = lambda x: x
+
         self.data = None
         self.label = None
         self.prepare()
@@ -13,9 +20,11 @@ class Dataset(ABC):
     def __getitem__(self, index):
         assert np.isscalar(index)
         if self.label is None:
-            return self.data[index], None
+            return self.transform(self.data[index]), None
         else:
-            return self.data[index], self.label[index]
+            return (
+                self.transform(self.data[index]),
+                self.target_transform(self.label[index]))
 
     def __len__(self):
         return len(self.data)
