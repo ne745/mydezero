a6edea1e0b7769de1763de82a0c0f9d0f711e38a
ne745 <kkpma0@gmail.com>
2022-10-22 21:07:39 +0900

    Step 51: ファイルをダウンロードする関数を実装

--- dezero/utils.py
+++ dezero/utils.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import urllib.request
 
 import numpy as np
 
@@ -108,3 +109,41 @@ def logsumexp(x, axis=1):
     np.log(s, out=s)
     m += s
     return m
+
+
+def show_progress(block_num, block_size, total_size):
+    bar_template = '\r[{}] {:.2f}%'
+
+    downloaded = block_num * block_size
+    p = downloaded / total_size * 100
+    i = downloaded // total_size * 30
+    p = min(p, 100.0)
+    i = min(i, 30)
+    bar = '#' * i + '.' * (30 - i)
+    print(bar_template.format(bar, p), end='')
+
+
+cache_dir = os.path.join(os.path.expanduser('~'), '.dezero')
+
+
+def get_file(url, file_name=None):
+    if file_name is None:
+        file_name = url[url.rfind('/') + 1:]
+    file_path = os.path.join(cache_dir, file_name)
+
+    if not os.path.exists(cache_dir):
+        os.mkdir(cache_dir)
+
+    if os.path.exists(file_path):
+        return file_path
+
+    print(f'Downloading: {file_name}')
+    try:
+        urllib.request.urlretrieve(url, file_path, show_progress)
+    except (Exception, KeyboardInterrupt) as e:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        raise e
+    print(' Done')
+
+    return file_path
